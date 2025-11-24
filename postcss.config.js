4520928fb187fcdcdff0e503ba2b516ad72e07f1
PAUL STROOT <paul@ocupop.com>
2025-11-24 09:52:43 -0600

    Fix for prohibition logo svg

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,11 @@
 /* eslint-env node */
-const tailwindcss = require('@tailwindcss/postcss');
 const clampwind = require('postcss-clampwind');
 
 module.exports = {
 	plugins: [
 		require('postcss-import-ext-glob'),
 		require('postcss-import'),
-		tailwindcss(),
+		require('@tailwindcss/postcss'),
 		require('autoprefixer'),
 		(clampwind.default ? clampwind.default : clampwind)(),
 	],
